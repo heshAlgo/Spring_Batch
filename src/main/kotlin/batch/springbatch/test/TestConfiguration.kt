aec06318a769b9915cdb753d79e7f80572b6c151
heshAlgo <serverdeveloper@naver.com>
2021-08-19 22:55:26 +0900

    주석추가

--- src/main/kotlin/batch/springbatch/test/TestConfiguration.kt
+++ src/main/kotlin/batch/springbatch/test/TestConfiguration.kt
@@ -6,7 +6,6 @@ import org.springframework.batch.core.StepContribution
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.core.scope.context.ChunkContext
-import org.springframework.batch.core.step.tasklet.Tasklet
 import org.springframework.batch.repeat.RepeatStatus
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -29,6 +28,7 @@ class TestConfiguration(
     @Bean
     fun step(): Step {
         return stepBuilderFactory.get("step1")
+            // Tasklet을 구현하면 스텝이 중지 될때까지 execute메서드가 계속 수행
             .tasklet { _: StepContribution, _: ChunkContext ->
                 println("Hello World")
                 RepeatStatus.FINISHED
