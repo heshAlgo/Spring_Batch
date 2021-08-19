4fc7660ee9c79763f83d2b16c2f556a28f615700
heshAlgo <serverdeveloper@naver.com>
2021-08-19 22:34:32 +0900

    어노테이션 추가

--- src/main/kotlin/batch/springbatch/SpringbatchApplication.kt
+++ src/main/kotlin/batch/springbatch/SpringbatchApplication.kt
@@ -1,8 +1,10 @@
 package batch.springbatch
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableBatchProcessing
 @SpringBootApplication
 class SpringbatchApplication
 
